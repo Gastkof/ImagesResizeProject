d20103d91583f82cfd3cb2ac963c9921f7b432ad
gaston kofmansky <gastkof1988@gmail.com>
2018-09-10 13:48:01 +0300

    update 10.9.2018

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const Jimp =require('jimp');
 var Fakerator = require("fakerator");
 var fs = require('fs');
 const settings =require("./settings.json")
-console.log(settings.result_folder)
+//const findUp = require('find-up');
+const isImage = require('is-image');
 
+console.log(settings.result_folder);
 
 
  
@@ -15,12 +17,14 @@ if (process.argv.length <= 2) {
     process.exit(-1);
 }
  
-if(!fs.exists("./")){
+// if(!fs.exists("./")){
 
-}
+// }
 
 var path = process.argv[2];
- 
+
+function reSize(path){
+
 fs.readdir(path, function(err, from) {
  
   //  console.log("my filez:",from);
@@ -33,13 +37,19 @@ var fakerator =Fakerator();
     console.log(from);
  
     for (var i=0; i<from.length; i++) {
+        //check if evrey file is a image
+      if( isImage(path.toString()+"/"+from[i].toString())){
         var name=fakerator.names.name();
+      
         resize(path+"/"+from[i],200,200,50,"./respond/"+name.toString()+".jpg");
+
+      }
+   
     }
 
-});
+})
  
-
+};
 
 // recive  the width and hight for resize a image  
 // also the quality and the diraction path with the full name of the resize image
@@ -59,7 +69,7 @@ function func1(a,b,c){
 
         var listOfArguments = process.argv
 
-        var ArratToCheck =["folder","fieName"]
+        var ArratToCheck =["demoFolder","respond"]
 
         let ObjectResult={}
         ObjectResult = func2(listOfArguments,ArratToCheck) 
@@ -104,15 +114,34 @@ function func2(listOfArguments,ArratToCheck){
    
     listOfArguments.forEach((val,i,arr)=> {
         
-
-        for(var j in ArratToCheck){
-            if(arr[i]===ArratToCheck[j]){
-                let ret = {}
-                ret[ArratToCheck[j].toString()] = arr[i+1]
-                
-                result.push(ret)
+        try{
+            //check if is the folder name
+            if(arr[i]===ArratToCheck[0]){
+                //check if the string entred is a folder
+            fs.lstatSync("./"+arr[i].toString()).isDirectory()
+            //call a function that resize the pictures
+            reSize("./"+arr[i].toString())
             }
-        }
+       }catch(e){
+          // Handle error
+          if(e.code == 'ENOENT'){
+        console.log("no such thing")
+          }else {
+        
+          }
+       }
+
+
+
+
+        // for(var j in ArratToCheck){
+        //     if(arr[i]===ArratToCheck[j]){
+        //         let ret = {}
+        //         ret[ArratToCheck[j].toString()] = arr[i+1]
+                
+        //         result.push(ret)
+        //     }
+        // }
 
     });
 
